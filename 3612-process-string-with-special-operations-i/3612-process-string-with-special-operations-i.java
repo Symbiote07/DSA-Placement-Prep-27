95a2b6d835c6ab8dad9fea1616c739bea3f193b3
Aryan shukla <153362878+Symbiote07@users.noreply.github.com>
2026-06-17 00:18:27 +0530

    Time: 4 ms (76.47%), Space: 55.3 MB (48.53%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder result=new StringBuilder();
+        char ch;
+        int l;
+        l=s.length();
+        for(int i=0;i<l;i++)
+        {
+            ch=s.charAt(i);
+            if(Character.isLowerCase(ch)== true)
+            {
+                result.append(ch);
+
+            }
+            else if(ch=='*')
+            {
+                if(result.length()>0)
+                {
+                    result.deleteCharAt(result.length()-1);
+                }
+            }
+            else if(ch=='#')
+            {
+                result.append(result.toString());
+            }
+            else if(ch=='%')
+            {
+                result.reverse();
+
+            }
+        }
+        return result.toString();
+    }
+}
