cde4b3126745b64bd88138b25e82935f0a0f587e
Aryan shukla <153362878+Symbiote07@users.noreply.github.com>
2026-06-17 14:24:14 +0530

    Time: 1 ms (98.22%), Space: 42.7 MB (25.05%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -2,13 +2,9 @@ class Solution {
     public int addDigits(int num) {
 
         if (num==0)
-        {
-            return 0;
-        }
-        else
-        {
-            return 1+((num-1)%9);
-        }
+        return 0;
+        return 1+((num-1)%9);
+    
 
     }
 }
