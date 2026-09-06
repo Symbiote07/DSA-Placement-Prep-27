8b164efa47358d1ebc4d374376425c08bc70d6b0
Aryan shukla <153362878+Symbiote07@users.noreply.github.com>
2026-09-06 16:20:21 +0530

    Time: 8 ms (92.72%), Space: 46.1 MB (45.97%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -10,3 +10,18 @@ class Solution {
         return false;
     }
 }
+
+// class Solution {
+//     public boolean isPowerOfThree(int n) {
+
+//         if (n == 1) {
+//             return true;
+//         }
+
+//         if (n <= 0 || n % 3 != 0) {
+//             return false;
+//         }
+
+//         return isPowerOfThree(n / 3);
+//     }
+// }
